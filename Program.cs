0790e25e376626d9a9d9a448174b1d5c0bfc2a5d
axl_vmo.msi <axl_vmo.msi@iides.cloud>
2023-03-11 21:15:23 -0600

    i_rev-002

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using ease_admin_cloud.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using AspNetCoreHero.ToastNotification;
+using AspNetCoreHero.ToastNotification.Extensions;
 
 namespace ease_admin_cloud
 {
@@ -11,17 +13,24 @@ namespace ease_admin_cloud
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+             var connectionString = builder.Configuration.GetConnectionString("pgSQLDataSource") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+                options.UseNpgsql(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                      builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
+            builder.Services.AddNotyf(config =>
+ {
+     config.DurationInSeconds = 5;
+     config.IsDismissable = true;
+     config.Position = NotyfPosition.BottomRight;
+ });
 
             var app = builder.Build();
-
+ AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -45,6 +54,7 @@ namespace ease_admin_cloud
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.UseNotyf();
             app.MapRazorPages();
 
             app.Run();
